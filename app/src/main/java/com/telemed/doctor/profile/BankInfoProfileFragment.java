bb5687a0d549bad00b5018dfebc09d028ed01be6
Parvinderandroid1 <parvinder.android1@outlook.com>
2019-11-13 13:04:41 +0530

    second commit

--- app/src/main/java/com/telemed/doctor/profile/BankInfoProfileFragment.java
+++ app/src/main/java/com/telemed/doctor/profile/BankInfoProfileFragment.java
@@ -3,6 +3,8 @@ package com.telemed.doctor.profile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -29,6 +31,12 @@ public class BankInfoProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_bank_info_profile, container, false);
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String a="This is my first code testing commit";
+    }
 }
